623897e84e38bbac936846bb6f7e987de5760034
Yves Michel ENYEGUE <danielngono002@gmail.com>
2026-02-11 09:09:59 +0100

    favicon d'affichage

--- src/app/Core/Constant/EndPoints.ts
+++ src/app/Core/Constant/EndPoints.ts
@@ -1,8 +1,8 @@
 import { Type } from "@angular/core";
 import { create } from "domain";
 
-//const api = "http://localhost:8080/edulearn/api" ; 
-const api = "https://edulearn-backend-8coh.onrender.com/edulearn/api"  ;           
+const api = "http://localhost:8080/edulearn/api" ; 
+//const api = "https://edulearn-backend-8coh.onrender.com/edulearn/api"  ;           
 const userapi = `${api}/user`; 
 const repetitionapi = `${api}/repetition`; 
 const marketplaceapi = `${api}/marketplace`; 
